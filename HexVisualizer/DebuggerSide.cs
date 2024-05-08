18c37b44bc9f88698b940d5e584bc90b988f280d
Andrew Korson <agkorson@charter.net>
2024-05-08 11:01:23 -0700

    Add project files.

---
+++ HexVisualizer/DebuggerSide.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.DebuggerVisualizers;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[assembly: System.Diagnostics.DebuggerVisualizer(
+typeof(HexVisualizer.DebuggerSide),
+typeof(VisualizerObjectSource),
+Target = typeof(byte[]),
+Description = "Byte Array Hex Visualizer")]
+
+namespace HexVisualizer {
+    public class DebuggerSide : DialogDebuggerVisualizer {
+        public DebuggerSide() : base(FormatterPolicy.Legacy) {
+            // nothing else to do
+        }
+
+        protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider) {
+            try {
+                IVisualizerObjectProvider3 objectProvider3 = (IVisualizerObjectProvider3)objectProvider;
+                byte[] bArray;
+
+                using (Stream opData = objectProvider3.GetData()) {
+                    BinaryFormatter bf = new BinaryFormatter();
+                    bArray = bf.Deserialize(opData) as byte[];
+                }
+                HexViewer hexViewer = new HexViewer {
+                    Text = "Array size is " + bArray.Length
+                };
+                windowService.ShowDialog(hexViewer);
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Error in visualizer: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ MyTestConsole/TestConsole.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HexVisualizer;
+using Microsoft.VisualStudio.DebuggerVisualizers;
+
+namespace MyTestConsole {
+    internal class TestConsole {
+
+        static void Main(string[] args) {
+            byte[] bTest = new byte[52];
+            for (int i = 0;  i < bTest.Length; i++) {
+                bTest[i] = (byte)(100-i);
+            }
+            byte[] cTest = new byte[100];
+
+            VisualizerDevelopmentHost visualizerHost = new VisualizerDevelopmentHost(null, typeof(DebuggerSide));
+            //visualizerHost.ShowVisualizer();
+            MessageBox.Show("stop here, test debugger");
+        }
+    }
+}
